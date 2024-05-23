6d2e1cfcb825a0640ee2fc6dbff0d92834a3e9fb
Himozaa <impocanal@gmail.com>
2024-05-24 01:50:56 +0500

    add login and logout

---
+++ CookingBlogFront/AuthorizeApi.cs
@@ -0,0 +1,26 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+
+public class AuthorizeApi 
+{
+	private readonly ILocalStorageService _localStorageService;
+	private IdentutyAuthenticationStateProvider _stateProvider;
+	public AuthorizeApi(AuthenticationStateProvider authenticationStateProvider, ILocalStorageService localStorage)
+	{
+		_stateProvider = (IdentutyAuthenticationStateProvider)authenticationStateProvider;
+		_localStorageService = localStorage;
+	}
+
+	public async Task<bool> Login(string login, string password)
+	{
+		var token = "awdwadwa";
+
+
+
+		await _localStorageService.SetItemAsStringAsync("token", token);
+
+		_stateProvider.MarkAsAuthenticated(token);
+
+		return true;
+	}
+}
---
+++ CookingBlogFront/IdentutyAuthenticationStateProvider.cs
@@ -0,0 +1,55 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+public class IdentutyAuthenticationStateProvider : AuthenticationStateProvider
+{
+	private readonly ILocalStorageService _localStorageService;
+	public IdentutyAuthenticationStateProvider(ILocalStorageService localStorage)
+	{
+		_localStorageService = localStorage;
+	}
+
+
+	public async override Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+		try
+		{
+
+			var token = await _localStorageService.GetItemAsStringAsync("token");
+			if (!string.IsNullOrEmpty(token))
+			{
+
+				MarkAsAuthenticated(token);
+
+				return await this.GetAuthenticationStateAsync();
+			}
+		}
+		catch (Exception ex) { }
+
+
+		return new AuthenticationState(new System.Security.Claims.ClaimsPrincipal(new ClaimsIdentity()));
+
+    }
+
+	internal void MarkAsAuthenticated(string token)
+	{
+		var authUser = new ClaimsPrincipal(new  ClaimsIdentity(
+			new List<Claim>()
+			{
+				new Claim(ClaimTypes.Name, "admin")
+			}, "jwt"));
+		var authState = Task.FromResult(new AuthenticationState(authUser));
+
+		NotifyAuthenticationStateChanged(authState);
+	}
+
+    public async Task MarkLogouted()
+    {
+        await _localStorageService.RemoveItemAsync("token");
+        NotifyAuthenticationStateChanged(Task.FromResult(Empty()));
+    }
+
+    private static AuthenticationState Empty()
+            => new(new ClaimsPrincipal(new ClaimsIdentity()));
+}
