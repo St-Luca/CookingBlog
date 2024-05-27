af8c43ed9ed12b8e1e8237ea98b6123574890ec0
Himozaa <impocanal@gmail.com>
2024-05-27 18:59:15 +0500

    make  RecipePage, RecipeService

---
+++ CookingBlogFront/Data/Recipe.cs
@@ -0,0 +1,9 @@
+﻿namespace CookingBlogFront.Data
+{
+    public class Recipe
+    {
+        public string title { get; set; }
+        public string instructions { get; set; }
+        public List<Ingredient> extendedIngredients { get; set; }
+    }
+}
