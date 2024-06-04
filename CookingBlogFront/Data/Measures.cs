44f3816134e3569515c3277155e375deb595eeae
Himozaa <impocanal@gmail.com>
2024-06-04 14:41:26 +0500

    make page Recipe

---
+++ CookingBlogFront/Data/Measures.cs
@@ -0,0 +1,8 @@
+﻿namespace CookingBlogFront.Data
+{
+    public class Measures
+    {
+        public IngredientUs us { get; set; }
+        public IngredientMetric metric { get; set; }
+    }
+}
--- CookingBlogFront/Data/Recipe.cs
+++ CookingBlogFront/Data/Recipe.cs
@@ -2,8 +2,11 @@
 {
     public class Recipe
     {
+        public int id { get; set; }
         public string title { get; set; }
         public string instructions { get; set; }
+        public string image { get; set; }
+        public double pricePerServing {  get; set; }
         public List<Ingredient> extendedIngredients { get; set; }
     }
 }
